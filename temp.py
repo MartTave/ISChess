c54f84ffb93dbd5973fc6831d596241463e4ea79
MartTave <martin.tavernier256@gmail.com>
2023-12-11 00:09:25 +0100

    Added queen moves. Added base for get all moves function

--- temp.py
+++ temp.py
@@ -1,12 +1,11 @@
-
-
+import copy
 from Board import Board
 
 # TODO Remove
 from heuristic import parseBoard
 
 def isInBoard(board:Board, x:int, y:int):
-    return x >= 0 and x < len(board.boardTab) and y >= 0 and y < len(board.boardTab)
+    return x >= 0 and x < len(board.boardTab[0]) and y >= 0 and y < len(board.boardTab)
 
 
 # This method return -1 is out of board
@@ -15,7 +14,7 @@ def isInBoard(board:Board, x:int, y:int):
 def checkCellsContent(board: Board, x: int, y:int, allies: list[str], enemies:list[str])->int:
     if not isInBoard(board, x, y):
         return -1
-    data = board.boardTab[x][y]
+    data = board.boardTab[y][x]
     if allies.count(data[1]) > 0:
         return 1
     if enemies.count(data[1]) > 0:
@@ -26,9 +25,9 @@ def checkCellsContent(board: Board, x: int, y:int, allies: list[str], enemies:li
 def movePiece(board: Board, startX: int, startY: int, endX:int, endY: int)->Board:
     if not isInBoard(board, startX, startY) or not isInBoard(board, endX, endY):
         print("[ERROR] Move piece with invalid position")
-    data = board.boardTab.copy()
-    data[endX][endY] = data[startX][startY]
-    data[startX][startY] = ""
+    data = copy.deepcopy(board.boardTab)
+    data[endY][endX] = data[startY][startX]
+    data[startY][startX] = ""
     return Board(data, "")
 
 def checkCell(board: Board, startX:int, startY:int, endX:int, endY:int, allies:list[str], enemies:list[str], resList:list[Board])->bool:
@@ -50,24 +49,24 @@ def getMovesRook(board:Board, x:int, y:int, player:str, allies: list[str], enemi
         print("[ERROR] Get moves from rook : index non valid (",x , ", ", y, ")")
         return []
     # If we're here -> position passed in parameter is in board
-    if data[x][y][1] != player[0]:
-        print("[ERROR] Get moves from rook : cell is not from player : ", data[x][y], " and ", player)
+    if len(data[y][x]) > 1 and data[y][x][1] != player[0]:
+        print("[ERROR] Get moves from rook : cell is not from player : ", data[y][x], " and ", player)
+        return
     allAllies = allies.copy()
     allAllies.append(player)
-    print("All allies is ", allAllies)
     # Now we can check for moves
     res = []
     # We need to check for four directions
-    for i in range(x + 1, len(data)):
+    for i in range(x + 1, len(data[0])):
         if not checkCell(board, x, y, i, y, allAllies, enemies, res):
             break
-    for i in range(x - 1, 0):
+    for i in range(x - 1, 0, -1):
         if not checkCell(board, x, y, i, y, allAllies, enemies, res):
             break
-    for i in range(y + 1, len(data[0])):
+    for i in range(y + 1, len(data)):
         if not checkCell(board, x, y, x, i, allAllies, enemies, res):
             break
-    for i in range(y - 1, 0):
+    for i in range(y - 1, 0, -1):
         if not checkCell(board, x, y, x, i, allAllies, enemies, res):
             break
     return res
@@ -79,12 +78,11 @@ def getMaladeMoves(board: Board, x:int, y:int, player:str, allies: list[str], en
         print("[ERROR] Get moves from rook : index non valid (",x , ", ", y, ")")
         return []
     # If we're here -> position passed in parameter is in board
-    if data[x][y][1] != player[0]:
-        print("[ERROR] Get moves from rook : cell is not from player : ", data[x][y], " and ", player) 
+    if len(data[y][x]) > 1 and data[y][x][1] != player[0]:
+        print("[ERROR] Get moves from rook : cell is not from player : ", data[y][x], " and ", player) 
         return []   
     allAllies = allies.copy()
     allAllies.append(player)
-    print("All allies is ", allAllies)
 
 
     res = []
@@ -125,7 +123,29 @@ def getMaladeMoves(board: Board, x:int, y:int, player:str, allies: list[str], en
         currentY -= 1
     return res
 
+def getQueenMoves(board: Board, x: int, y: int, player:str, allies:list[str], enemies: list[str]) -> list[Board]:
+    part1 = getMovesRook(board, x, y, player, allies, enemies)
+    part2 = getMaladeMoves(board, x, y, player, allies, enemies)
+    return part1 + part2
+
+def getAllMoves(board: Board, player: str, allies: list[str], enemies: list[str])->list[Board]:
+    res = []
+    for y in range(0, len(board.boardTab)):
+        for x in range(0, len(board.boardTab[y])):
+            current = board.boardTab[y][x]
+            if len(current) > 1 and current[1] == player:
+                # Cells has a piece and it's ours !
+                piece = current[0]
+                match piece:
+                    case 'q':
+                        res += getQueenMoves(board, x, y, player, allies, enemies)
+                    case 'r':
+                        res += getMovesRook(board, x, y, player, allies, enemies)
+                    case 'b':
+                        res += getMaladeMoves(board, x, y, player, allies, enemies)
+
+
 baseBoard = parseBoard("./Data/maps/default.brd")
 
-res = getMaladeMoves(baseBoard, 0, 2, "w", [], ["b"])
+res = getQueenMoves(baseBoard, 3, 0, "w", [], ["b"])
 print(len(res))
